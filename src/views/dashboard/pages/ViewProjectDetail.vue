b817d444564bec73065ffda655183736917ec449
vannlse130680 <nguyenlamvan1999@gmail.com>
2021-01-26 21:48:30 +0700

    test git 2

---
+++ src/views/dashboard/pages/ViewProjectDetail.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-container
+    id="user-profile"
+    fluid
+    tag="section"
+  >
+    <v-row justify="center">
+      <v-col
+        cols="12"
+        md="12"
+      >
+        <base-material-card>
+          <template v-slot:heading>
+            <div class="display-2 font-weight-light">
+              Project Information
+            </div>
+
+            <!-- <div class="subtitle-1 font-weight-light">
+              Complete your profile
+            </div> -->
+          </template>
+
+          <v-form>
+            <v-container class="py-0">
+              <v-row>
+                <v-col
+                  cols="12"
+                >
+                  <v-text-field
+                    v-model="first"
+                    label="Project Code"
+                    filled
+                  />
+
+                  <v-text-field
+                    v-model="first"
+                    label="Project Name"
+                    filled
+                  />
+                </v-col>
+                <v-col>
+                  <v-btn
+                    absolute
+                    right
+                    bottom
+                    tile
+                    color="success"
+                  >
+                    <v-icon left>
+                      mdi-pencil
+                    </v-icon>
+                    Create
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </base-material-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      items: ['VFRAM', 'FPT', 'ABC', 'Buzz'],
+    }),
+  }
+</script>
