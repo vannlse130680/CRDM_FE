2c891e0c9d107e12b2c497fb0988d57870acc2e3
vannlse130680 <nguyenlamvan1999@gmail.com>
2021-01-27 10:38:17 +0700

    update new

--- src/views/dashboard/tables/ViewProject.vue
+++ src/views/dashboard/tables/ViewProject.vue
@@ -20,7 +20,106 @@
         </v-btn>
       </v-col>
     </v-row>
-
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="600px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">Project Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal first name*"
+                  required
+                />
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal middle name"
+                  hint="example of helper text only on focus"
+                />
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+                md="4"
+              >
+                <v-text-field
+                  label="Legal last name*"
+                  hint="example of persistent helper text"
+                  persistent-hint
+                  required
+                />
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email*"
+                  required
+                />
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  label="Password*"
+                  type="password"
+                  required
+                />
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-select
+                  :items="['0-17', '18-29', '30-54', '54+']"
+                  label="Age*"
+                  required
+                />
+              </v-col>
+              <v-col
+                cols="12"
+                sm="6"
+              >
+                <v-autocomplete
+                  :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
+                  label="Interests"
+                  multiple
+                />
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     <base-material-card
       id="btn-create"
       color="success"
@@ -40,16 +139,13 @@
         </thead>
 
         <tbody>
-          <router-link
-            to="/project/view-project-detail"
-            tag="tr"
-          >
+          <tr>
             <td>1</td>
             <td>AX001</td>
             <td>Niger</td>
             <td>Oud-Turnhout</td>
             <td>2020/01/01</td>
-          </router-link>
+          </tr>
           <tr>
             <td>1</td>
             <td>AX001</td>
@@ -102,6 +198,15 @@
     </base-material-card>
   </v-container>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+  }
+</script>
 <style>
 #btn-create {
   margin-top: 50px;
