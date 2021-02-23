ac373d17b61d73383843ab01f9e6844c12f87721
vannlse130680 <nguyenlamvan1999@gmail.com>
2021-02-23 17:14:12 +0700

    update nha hien

---
+++ src/views/dashboard/tables/MaterialDetails.vue
@@ -0,0 +1,229 @@
+<!--<template>
+  <v-container
+    id="data-tables"
+    tag="section"
+  >
+    <v-row>
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-text-field
+          label="Project Code"
+          value="PRO001"
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-text-field
+          label="Product"
+          value="Coconut Soap"
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-text-field
+          label="Client"
+          value="ABC Cosmetics"
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-combobox
+          v-model="select"
+          :items="items"
+          label="Assignee"
+          multiple
+          chips
+        >
+          <template v-slot:selection="data">
+            <v-chip
+              :key="JSON.stringify(data.item)"
+              v-bind="data.attrs"
+              :input-value="data.selected"
+              :disabled="data.disabled"
+              @click:close="data.parent.selectItem(data.item)"
+            >
+              <v-avatar
+                class="accent white--text"
+                left
+                v-text="data.item.slice(0, 1).toUpperCase()"
+              />
+              {{ data.item }}
+            </v-chip>
+          </template>
+        </v-combobox>
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-text-field
+          label="Create date"
+          placeholder="2021/01/26"
+          disabled
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-text-field
+          label="Reporter"
+          value="Manager"
+          disabled
+        />
+      </v-col>
+
+      <v-col
+        cols="12"
+        sm="6"
+        md="3"
+      >
+        <v-text-field
+          label="Requirment"
+          textarea
+          outline
+        />
+      </v-col>
+
+      <base-material-card
+        icon="mdi-vuetify"
+        inline
+        class="px-5 py-3"
+      >
+        <template v-slot:after-heading>
+          <div class="display-2 font-weight-light">
+            Formula Version
+          </div>
+        </template>
+
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          class="ml-auto"
+          label="Search"
+          hide-details
+          single-line
+          style="max-width: 250px;"
+        />
+
+        <v-divider class="mt-3" />
+
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          :search.sync="search"
+          :sort-by="['name', 'office']"
+          :sort-desc="[false, true]"
+          multi-sort
+        />
+      </base-material-card>
+    </v-row>
+  </v-container>
+</template>
+<script>
+  export default {
+    name: 'DashboardDataTables',
+
+    data: () => ({
+      headers: [
+        {
+          text: 'Name',
+          value: 'name',
+        },
+        {
+          text: 'Position',
+          value: 'position',
+        },
+        {
+          text: 'Office',
+          value: 'office',
+        },
+        {
+          text: 'Age',
+          value: 'age',
+        },
+        {
+          text: 'Date',
+          value: 'date',
+        },
+        {
+          sortable: false,
+          text: 'Actions',
+          value: 'actions',
+        },
+      ],
+      items: [
+        {
+          name: 'Airi Satou',
+          position: 'Accountant',
+          office: 'Tokyo',
+          age: 33,
+          date: '2008/11/28',
+        },
+        {
+          name: 'Angelica Ramos',
+          position: 'Chief Executive Officer (CEO)',
+          office: 'London',
+          age: 47,
+          date: '2009/10/09',
+        },
+        {
+          name: 'Ashton Cox',
+          position: 'Junior Technical Author',
+          office: 'San Francisco',
+          age: 66,
+          date: '2009/01/12',
+        },
+        {
+          name: 'Bradley Greer',
+          position: 'Software Engineer',
+          office: 'London',
+          age: 41,
+          date: '2012/10/13',
+        },
+        {
+          name: 'Brenden Wagner',
+          position: 'Software Engineer',
+          office: 'San Francisco',
+          age: 28,
+          date: '2011/06/07',
+        },
+      ],
+      search: undefined,
+    }),
+  }
+</script>
+<script>
+  export default {
+    data () {
+      return {
+        select: ['DucPLM', 'LinhLNK'],
+        items: [
+          'DucPLM',
+          'VanNL',
+          'SuNV',
+          'LinhLNK',
+        ],
+      }
+    },
+  }
+</script>-->
---
+++ src/views/dashboard/tables/Supplier.vue
@@ -0,0 +1,261 @@
+<template>
+  <base-material-card
+    color="success"
+    icon="mdi-basket-fill"
+    title="Supplier List"
+    class="px-5 py-3"
+  >
+    <v-data-table
+      :search="search"
+      :headers="headers"
+      :items="desserts"
+      sort-by="calories"
+      class="elevation-1"
+    >
+      <template v-slot:top>
+        <v-toolbar
+          flat
+        >
+          <!-- ?<v-toolbar-title>Material</v-toolbar-title> -->
+          <v-divider
+            class="mx-4"
+            inset
+            vertical
+          />
+          <v-spacer />
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+            class="mr-10"
+          />
+          <v-dialog
+            v-model="dialog"
+            max-width="500px"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                color="primary"
+                dark
+                class="mb-2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                Add New
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title>
+                <span class="headline">{{ formTitle }}</span>
+              </v-card-title>
+
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                    >
+                      <v-text-field
+                        v-model="editedItem.supplier"
+                        label="Supplier"
+                      />
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
+
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="close"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="save"
+                >
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <v-dialog
+            v-model="dialogDelete"
+            max-width="500px"
+          >
+            <v-card>
+              <v-card-title class="headline">
+                Are you sure you want to delete this item?
+              </v-card-title>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="closeDelete"
+                >
+                  Cancel
+                </v-btn>
+                <v-btn
+                  color="blue darken-1"
+                  text
+                  @click="deleteItemConfirm"
+                >
+                  OK
+                </v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-toolbar>
+      </template>
+      <template
+        v-slot:[`item.actions`]="{ item }"
+        class="mr-10"
+      >
+        <v-icon
+          small
+          class="mr-2"
+          @click="editItem(item)"
+        >
+          mdi-pencil
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item)"
+        >
+          mdi-delete
+        </v-icon>
+      </template>
+      <template v-slot:no-data>
+        <v-btn
+          color="primary"
+          @click="initialize"
+        >
+          Reset
+        </v-btn>
+      </template>
+    </v-data-table>
+  </base-material-card>
+  <!-- </v-data-table> -->
+</template>
+<script>
+  export default {
+    data: () => ({
+      search: '',
+      dialog: false,
+      dialogDelete: false,
+      headers: [
+        {
+          text: 'Supplier',
+          align: 'start',
+          sortable: true,
+          value: 'supplier',
+        },
+        { text: 'Actions', value: 'actions', sortable: false, align: 'center' },
+      ],
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: '',
+      },
+      defaultItem: {
+        name: '',
+      },
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+      this.initialize()
+    },
+
+    methods: {
+      initialize () {
+        this.desserts = [
+          {
+            supplier: 'Công ty TNHH La Vie',
+          },
+          {
+            supplier: 'Công ty TNHH La Vie',
+          },
+          {
+            supplier: 'Công ty TNHH La Vie',
+          },
+          {
+            supplier: 'Công ty TNHH La Vie',
+          },
+          {
+            supplier: 'Công ty TNHH La Vie',
+          },
+          {
+            supplier: 'Công ty TNHH La Vie',
+          },
+        ]
+      },
+
+      editItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.desserts.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.desserts.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.desserts[this.editedIndex], this.editedItem)
+        } else {
+          this.desserts.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
+</script>
